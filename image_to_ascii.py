d6bea7041b57b8a7c22953c44cbf51d05aeb5197
BSmith156 <bensmith156@sky.com>
2021-05-25 23:38:35 +0100

    Add command-line arguments

--- image_to_ascii.py
+++ image_to_ascii.py
@@ -1,1 +1,23 @@
-print("Hello World!")
+import sys
+
+usage = "Usage: image_to_ascii.py input_file output_file [-i]\n\t-i: Invert output colour. Useful when output is displayed using a light font and dark background."
+
+# Check command-line arguments
+args = sys.argv
+argsNum = len(args)
+
+if(argsNum == 1):
+    print(f"\n{usage}\n")
+    quit()
+
+if(argsNum == 2 or argsNum > 4):
+    print(f"\nInvalid number of arguments\n\n{usage}\n")
+    quit()
+
+invert = False
+if(argsNum == 4):
+    if(args[3] != "-i"):
+        print(f"\nInvalid argument: {args[3]}\n\n{usage}\n")
+        quit()
+    else:
+        invert = True
